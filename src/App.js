50d57f274654fafaa3529f5474b9d0fad4742322
lucascr <lcrodrigues1995@gmail.com>
2022-08-03 15:26:33 -0300

    Return calculator on App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
+      <Calculator />
     </div>
   );
 }
