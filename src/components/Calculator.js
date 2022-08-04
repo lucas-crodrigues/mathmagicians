87dbf980718fcdf29d7184a2123bcca79017a005
lucascr <lcrodrigues1995@gmail.com>
2022-08-04 15:38:35 -0300

    Refactor calculator using hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,53 +1,42 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './CalculatorStyle.css';
 import calculate from './logic/calculate';
 
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      states: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-    };
-    this.calculate = this.calculate.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    next: null, total: null, operation: null, err: null,
+  });
 
-  calculate(e) {
-    const { states } = this.state;
-    const newState = calculate(states, e.target.textContent);
-    this.setState({ states: newState });
-  }
+  const calculation = (e) => {
+    setState((state) => calculate(state, e.target.textContent));
+  };
 
-  render() {
-    const { states } = this.state;
-    return (
-      <section className="calculator">
-        <div className="output">{states.next || states.total || 0}</div>
-        <div className="buttons">
-          <button type="button" className="bt_clear" onClick={this.calculate}>AC</button>
-          <button type="button" className="bt_sign" onClick={this.calculate}>+/-</button>
-          <button type="button" className="bt_percentage" onClick={this.calculate}>%</button>
-          <button type="button" className="bt_division right_column" onClick={this.calculate}>÷</button>
-          <button type="button" className="bt_7" onClick={this.calculate}>7</button>
-          <button type="button" className="bt_8" onClick={this.calculate}>8</button>
-          <button type="button" className="bt_9" onClick={this.calculate}>9</button>
-          <button type="button" className="bt_times right_column" onClick={this.calculate}>x</button>
-          <button type="button" className="bt_4" onClick={this.calculate}>4</button>
-          <button type="button" className="bt_5" onClick={this.calculate}>5</button>
-          <button type="button" className="bt_6" onClick={this.calculate}>6</button>
-          <button type="button" className="bt_minus right_column" onClick={this.calculate}>-</button>
-          <button type="button" className="bt_1" onClick={this.calculate}>1</button>
-          <button type="button" className="bt_2" onClick={this.calculate}>2</button>
-          <button type="button" className="bt_3" onClick={this.calculate}>3</button>
-          <button type="button" className="bt_plus right_column" onClick={this.calculate}>+</button>
-          <button type="button" className="bt_0" onClick={this.calculate}>0</button>
-          <button type="button" className="bt_dot" onClick={this.calculate}>.</button>
-          <button type="button" className="bt_equal right_column" onClick={this.calculate}>=</button>
-        </div>
-      </section>
-    );
-  }
-}
+  return (
+    <section className="calculator">
+      <div className="output">{state.next || state.operation || state.total || state.err || 0}</div>
+      <div className="buttons">
+        <button type="button" className="bt_clear" onClick={calculation}>AC</button>
+        <button type="button" className="bt_sign" onClick={calculation}>+/-</button>
+        <button type="button" className="bt_percentage" onClick={calculation}>%</button>
+        <button type="button" className="bt_division right_column" onClick={calculation}>÷</button>
+        <button type="button" className="bt_7" onClick={calculation}>7</button>
+        <button type="button" className="bt_8" onClick={calculation}>8</button>
+        <button type="button" className="bt_9" onClick={calculation}>9</button>
+        <button type="button" className="bt_times right_column" onClick={calculation}>x</button>
+        <button type="button" className="bt_4" onClick={calculation}>4</button>
+        <button type="button" className="bt_5" onClick={calculation}>5</button>
+        <button type="button" className="bt_6" onClick={calculation}>6</button>
+        <button type="button" className="bt_minus right_column" onClick={calculation}>-</button>
+        <button type="button" className="bt_1" onClick={calculation}>1</button>
+        <button type="button" className="bt_2" onClick={calculation}>2</button>
+        <button type="button" className="bt_3" onClick={calculation}>3</button>
+        <button type="button" className="bt_plus right_column" onClick={calculation}>+</button>
+        <button type="button" className="bt_0" onClick={calculation}>0</button>
+        <button type="button" className="bt_dot" onClick={calculation}>.</button>
+        <button type="button" className="bt_equal right_column" onClick={calculation}>=</button>
+      </div>
+    </section>
+  );
+};
+
+export default Calculator;
