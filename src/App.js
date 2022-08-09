cd9e0dac30ca6e0106b9a13a545177d787de9087
lucascr <lcrodrigues1995@gmail.com>
2022-08-09 12:52:12 -0300

    add markup to calculator page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
 
 const App = () => (
   <div className="App">
+    <header>
+      <h1>Math Magicians</h1>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <nav>
+        <Link to="/calculator">Calculator</Link>
+      </nav>
+      <nav>
+        <Link to="/quote">Quote</Link>
+      </nav>
+    </header>
+    <h3>Let&apos;s do some math!</h3>
     <Calculator />
   </div>
 );
