298089e40176a77c36514728730402c5d1ae4103
lucascr <lcrodrigues1995@gmail.com>
2022-08-09 14:42:01 -0300

    Update styling for the calculator section

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import { Link } from 'react-router-dom';
+import Header from './components/Header';
 import Calculator from './components/Calculator';
+import Footer from './components/Footer';
+import './Page.css';
 
 const App = () => (
-  <div className="App">
-    <header>
-      <h1>Math Magicians</h1>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-      <nav>
-        <Link to="/calculator">Calculator</Link>
-      </nav>
-      <nav>
-        <Link to="/quote">Quote</Link>
-      </nav>
-    </header>
-    <h3>Let&apos;s do some math!</h3>
-    <Calculator />
+  <div className="app">
+    <Header />
+    <h3
+      style={{
+        margin: '0 3vw',
+        fontSize: '30px',
+      }}
+    >
+      Let&apos;s do some math!
+    </h3>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row-reverse',
+      }}
+    >
+      <div
+        style={{
+          padding: '0 5vw 5vh 0',
+          height: '75vh',
+          width: '65vw',
+        }}
+      >
+        <Calculator />
+      </div>
+    </div>
+    <Footer />
   </div>
 );
 
