a5375b09ec02375bd4a28e5caa455c65187b6199
lucascr <lcrodrigues1995@gmail.com>
2022-08-09 14:43:11 -0300

    Update header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,14 @@ const Header = () => (
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
+      padding: '4vh 2vw',
+      backgroundColor: 'lightgray',
+      borderBottom: '1px solid gray',
     }}
   >
     <h1
       style={{
-        marginLeft: '2vw',
+        fontSize: '40px',
       }}
     >
       Math Magicians
@@ -26,20 +29,35 @@ const Header = () => (
       <nav
         style={{
           cursor: 'pointer',
+          fontSize: '20px',
         }}
       >
         <Link to="/">Home</Link>
       </nav>
+      <div
+        style={{
+          borderLeft: '1px solid gray',
+          height: '5vh',
+        }}
+      />
       <nav
         style={{
           cursor: 'pointer',
+          fontSize: '20px',
         }}
       >
         <Link to="/calculator">Calculator</Link>
       </nav>
+      <div
+        style={{
+          borderLeft: '1px solid gray',
+          height: '5vh',
+        }}
+      />
       <nav
         style={{
           cursor: 'pointer',
+          fontSize: '20px',
         }}
       >
         <Link to="/quote">Quote</Link>
