074753c22d3953dbb644bb3f2e047d0a6075b242
lucascr <lcrodrigues1995@gmail.com>
2022-08-10 20:23:05 -0300

    fix linters

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import App from '../App';
 
@@ -11,13 +11,13 @@ describe('Calculator page test suites', () => {
 
   test('Check calculator page title', () => {
     render(<BrowserRouter><App /></BrowserRouter>);
-    const calculatorHeading = screen.getByRole('heading', {  name: /let's do some math!/i});
+    const calculatorHeading = screen.getByRole('heading', { name: /let's do some math!/i });
     expect(calculatorHeading).toHaveTextContent('Let\'s do some math!');
   });
 
   test('Check calculator presence', () => {
     render(<BrowserRouter><App /></BrowserRouter>);
-    const buttonAC = screen.getByRole('button', {  name: /ac/i});
+    const buttonAC = screen.getByRole('button', { name: /ac/i });
     expect(buttonAC).toBeInTheDocument();
   });
 });
