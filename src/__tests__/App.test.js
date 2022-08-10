81ce1a3f714bc901c6759aeec1e2376e63998321
lucascr <lcrodrigues1995@gmail.com>
2022-08-10 20:18:04 -0300

    Update Calculator page tests to use snapshot

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react";
+import renderer from 'react-test-renderer';
 import App from '../App';
 
 describe('Calculator page test suites', () => {
+  test('Calculator page snapshot', () => {
+    const renderedComponent = renderer.create(<BrowserRouter><App /></BrowserRouter>).toJSON();
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
   test('Check calculator page title', () => {
     render(<BrowserRouter><App /></BrowserRouter>);
     const calculatorHeading = screen.getByRole('heading', {  name: /let's do some math!/i});
