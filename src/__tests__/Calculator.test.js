a09f6a84b1b5e88e1c716710e4d3dbb228b52e94
lucascr <lcrodrigues1995@gmail.com>
2022-08-11 08:47:19 -0300

    fix linters

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 
-
 describe('Calculator test suites', () => {
   test('Calculator screen', () => {
     render(<Calculator />);
